35a9abe620069d50addc7e886b22e61ae1335e49
Bereket Haileslassie B <bhbbereket@gmail.com>
2022-09-06 23:50:10 -0500

    service configuration done:

--- src/main/java/com/bereket/springbatchpre/service/Service.java
+++ src/main/java/com/bereket/springbatchpre/service/Service.java
@@ -1,7 +1,25 @@
 package com.bereket.springbatchpre.service;
+
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+
 @org.springframework.stereotype.Service
 public class Service {
+    @Autowired
+    private JobLauncher jobLauncher;
+    @Autowired
+    private Job job;
     public void getJobDone(){
-
+        JobParameters jobParameters=new JobParametersBuilder()
+                .addLong("startAt",System.currentTimeMillis()).toJobParameters();
+        try {
+            jobLauncher.run(job,jobParameters);
+        }catch (JobExecutionAlreadyRunningException | JobInstanceAlreadyCompleteException | JobRestartException | JobParametersInvalidException e) {
+            e.printStackTrace();
+        }
     }
 }
